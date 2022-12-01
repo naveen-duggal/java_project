4b0fe93e2ba41902b7f2d74fd2ea5e34898ff3a9
naveen-duggal <84651021+naveen-duggal@users.noreply.github.com>
2022-12-01 15:04:34 +0530

    Add files via upload

---
+++ Bank_Atm_Project/BankAtm.java
@@ -0,0 +1,93 @@
+import java.util.Scanner;
+
+class InvalidCustomerException extends Exception
+{
+    public InvalidCustomerException(String msg)
+    {
+        super(msg);
+    }
+}
+
+class Atm
+{
+    int userid = 1612;
+    int password = 1001;
+    int uid;
+    int pw;
+
+    public void input()
+    {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Kindly enter the user id ");
+        uid = scan.nextInt();
+
+        System.out.println("Kindly enter the password ");
+        pw = scan.nextInt();
+    }
+
+    public void verify() throws InvalidCustomerException
+    {
+        if((userid==uid) && (password==pw))
+        {
+            System.out.println();
+        }
+        else
+        {
+            InvalidCustomerException pas = new InvalidCustomerException("Invalid input Please try again");
+            System.out.println(pas.getMessage());
+            throw pas;
+        }
+    }
+}
+
+class Bank
+{
+    public void initiate()
+    {
+        Atm a = new Atm();
+        try
+        {
+        a.input();
+        a.verify();
+        }
+        catch(InvalidCustomerException e)
+        {
+            try
+            {
+                a.input();
+                a.verify();
+            }
+            catch(InvalidCustomerException f)
+            {
+                try
+                {
+                    a.input();
+                    a.verify();
+                }
+                catch(InvalidCustomerException g)
+                {
+                    try
+                    {
+                        a.input();
+                        a.verify();
+                    }
+                    catch(InvalidCustomerException h)
+                    {
+                        System.out.println("Your card has been blocked");
+                        System.exit(0);
+                    }
+                }
+            }
+        }
+    }
+}
+
+public class BankAtm
+{
+    public static void main(String[] args)
+    {
+        Bank b = new Bank();
+        b.initiate();
+    }
+} 
